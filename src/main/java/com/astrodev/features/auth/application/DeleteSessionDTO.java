6f7e25c55bcf317473bcb5397f89778dc4443df1
4strodev <contact@4strodev.com>
2026-01-05 22:49:28 +0100

    feat: add endpoint to delete session

---
+++ src/main/java/com/astrodev/features/auth/application/DeleteSessionDTO.java
@@ -0,0 +1,4 @@
+package com.astrodev.features.auth.application;
+
+public record DeleteSessionDTO(String refreshToken) {
+}
--- src/main/java/com/astrodev/features/auth/infrastructure/http/AuthSessionResource.java
+++ src/main/java/com/astrodev/features/auth/infrastructure/http/AuthSessionResource.java
@@ -1,10 +1,8 @@
 package com.astrodev.features.auth.infrastructure.http;
 
-import com.astrodev.features.auth.application.AuthService;
-import com.astrodev.features.auth.application.AuthSessionTokenStore;
-import com.astrodev.features.auth.application.CreateSessionDTO;
-import com.astrodev.features.auth.application.RefreshSessionDTO;
+import com.astrodev.features.auth.application.*;
 import com.astrodev.features.auth.infrastructure.http.dtos.SessionCreateResDTO;
+import com.astrodev.features.auth.infrastructure.http.dtos.SessionDeleteReqDTO;
 import com.astrodev.features.auth.infrastructure.http.dtos.SessionRefreshReqDTO;
 import com.astrodev.features.auth.infrastructure.http.dtos.SessionRefreshResDTO;
 import com.astrodev.shared.http.HttpErrorDetails;
@@ -14,6 +12,7 @@ import com.astrodev.shared.monads.Err;
 import com.astrodev.shared.monads.Ok;
 import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -51,6 +50,19 @@ public class AuthSessionResource {
                     HttpErrorDetails.fromThrowable(error)
             ));
             case Ok(var tokens) -> HttpResponse.success(new SessionRefreshResDTO(tokens.accessToken()));
+        };
+    }
+
+    @DELETE
+    @PermitAll
+    public HttpResponse deleteSession(SessionDeleteReqDTO sessionDeleteReqDTO) {
+        var result = this.authService.deleteSession(new DeleteSessionDTO(sessionDeleteReqDTO.refreshToken()));
+        return switch (result) {
+            case Err(var error) -> HttpResponse.error(new HttpErrorResponseData(
+                    null,
+                    HttpErrorDetails.fromThrowable(error)
+            ));
+            case Ok(var _) -> HttpResponse.success(null);
         };
     }
 }
